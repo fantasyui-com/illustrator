74db828dfa021b01978fed23768190c834e32ccf
Captain Fantasy <captain@fantasyui.com>
2017-05-15 11:10:22 -0400

    Expand Filter Selection

--- index.js
+++ index.js
@@ -11,11 +11,6 @@ const spawn = require('child_process').spawn;
 
 const filter = function(name){return require(path.join( __dirname, "filters", name+".js"))}
 
-const normalizeImage = filter("normalize-image");
-const charcoalDrawing = filter("charcoal-drawing");
-const sketchOutline = filter("sketch-outline");
-const findShadows = filter("find-shadows");
-const showSteps = filter("show-steps");
 
 async function main(files){
 
@@ -24,26 +19,54 @@ async function main(files){
   let chain = Promise.resolve();
   files.forEach(function (file) {
     const fullPath = path.resolve(file);
-    chain = chain.then(()=>normalizeImage(fullPath)).then(source => album.data.push({name:path.basename(file,path.extname(file)), source}))
-    chain = chain.then(()=>charcoalDrawing(fullPath)).then(source => album.data.push({name:path.basename(file,path.extname(file)), source}))
-    chain = chain.then(()=>sketchOutline(fullPath)).then(source => album.data.push({name:path.basename(file,path.extname(file)), source}))
-    chain = chain.then(()=>showSteps(fullPath)).then(source => album.data.push({name:path.basename(file,path.extname(file)), source}))
-    chain = chain.then(()=>findShadows(fullPath)).then(source => album.data.push({name:path.basename(file,path.extname(file)), source}))
+
+    chain = chain.then(()=>{if(program.verbose) console.log(`Processing: ${fullPath}`)} )
+
+    chain = chain.then(()=>filter("original-image")(fullPath)).then(source => album.data.push({name:path.basename(file,path.extname(file)), source}))
+    chain = chain.then(()=>filter("charcoal-drawing")(fullPath)).then(source => album.data.push({name:path.basename(file,path.extname(file)), source}))
+
+    chain = chain.then(()=>filter("sketch-one")(fullPath)).then(source => album.data.push({name:path.basename(file,path.extname(file)), source}))
+    chain = chain.then(()=>filter("sketch-two")(fullPath)).then(source => album.data.push({name:path.basename(file,path.extname(file)), source}))
+    chain = chain.then(()=>filter("sketch-three")(fullPath)).then(source => album.data.push({name:path.basename(file,path.extname(file)), source}))
+
+    chain = chain.then(()=>filter("three-shades")(fullPath)).then(source => album.data.push({name:path.basename(file,path.extname(file)), source}))
+    chain = chain.then(()=>filter("five-shades")(fullPath)).then(source => album.data.push({name:path.basename(file,path.extname(file)), source}))
+    chain = chain.then(()=>filter("seven-shades")(fullPath)).then(source => album.data.push({name:path.basename(file,path.extname(file)), source}))
+
+    chain = chain.then(()=>filter("base-shadows")(fullPath)).then(source => album.data.push({name:path.basename(file,path.extname(file)), source}))
+    chain = chain.then(()=>filter("dark-shadows")(fullPath)).then(source => album.data.push({name:path.basename(file,path.extname(file)), source}))
+    chain = chain.then(()=>filter("light-shadows")(fullPath)).then(source => album.data.push({name:path.basename(file,path.extname(file)), source}))
+
+
   });
   await chain;
 
   if(program.album){
+
+    const albumPath = path.resolve(program.album); // resolves using CWD
+
     if(program.unpdf){
-      const albumPath = path.join('.', program.album);
+
+      // store raw files in a directory
       await makeDir(albumPath);
+      if(program.verbose) console.log(`Album directory path: ${albumPath}`)
       album.data.forEach(function (file, index) {
-      fs.renameSync( file.source, path.join(albumPath, file.name +'.'+ index + path.extname(file.source)) );
+        const extName = path.extname(file.source);
+        const fileName = `${file.name}.${index}${extName}`;
+        const dest = path.join(albumPath, fileName)
+        fs.renameSync( file.source, dest );
       });
+
     }else{
-      const convert = spawn('convert', album.data.map(i=>i.source).concat([`./${program.album}.pdf`]));
-      convert.stdout.on('data', (data) => {});
-      convert.stderr.on('data', (data) => {});
-      convert.on('close', (code) => {});
+
+      // store pdf
+      const dest = albumPath + '.pdf'; // we add .ext to convert /home/bob/my-album to /home/bob/my-album.pdf
+      if(program.verbose) console.log(`Album PDF path: ${dest}`)
+      const exec = spawn('convert', album.data.map(i=>i.source).concat([dest]));
+      exec.stdout.on('data', (data) => {});
+      exec.stderr.on('data', (data) => {});
+      exec.on('close', (code) => {});
+
     }
   }
 }
@@ -51,6 +74,7 @@ async function main(files){
 
 program
   .version('0.0.1')
+  .option('-v, --verbose')
   .option('--album [name]', 'Album name [my-album]', 'my-album')
   .option('--unpdf', 'Disable pdf mode and use a simple directory.')
   .command('sketch <dir> [files...]')
